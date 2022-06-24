ca584b5093b1d7c1d6a8d1533ad2e684ffcaa9ae
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2022-06-24 23:48:36 +0100

    Working on the intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,25 @@
 import "./intro.scss";
 
 const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/Picture2.png" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, i'm</h2>
+          <h1>Ojotule Benjamin Ajodo</h1>
+          <h3>
+            Freelance<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">down icon from material ui</a>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
