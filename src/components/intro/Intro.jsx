3683e8b148b51162dd6d741baa5e3764a545a0e0
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2022-07-17 12:33:59 +0100

    Added the portfolio feature

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,26 @@
+// import { init } from "ityped";
+// import { useEffect, useRef } from "react";
+import TypeWriterEffect from "react-typewriter-effect";
 import down from "../../assets/down.png";
 import "./intro.scss";
 
 const Intro = () => {
+  // const wordsRef = useRef();
+  // useEffect(() => {
+
+  // })
+
+  // const textRef = useRef();
+
+  // useEffect(() => {
+  //   init(textRef.current, {
+  //     showCursor: true,
+  //     backDelay: 1500,
+  //     backSpeed: 60,
+  //     strings: ["Developer", "Writer"],
+  //   });
+  // }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,8 +33,25 @@ const Intro = () => {
           <h2>Hi There, i'm</h2>
           <h1>Ojotule Benjamin Ajodo</h1>
           <h3>
-            Freelance<span></span>
+            Freelance{" "}
+            <span>
+              <TypeWriterEffect
+                textStyle={{
+                  fontSize: "30px",
+                  color: "crimson",
+                }}
+                startDelay={100}
+                cursorColor="crimson"
+                multiText={["Developer", "Content Writer", "Designer"]}
+                multiTextDelay={1000}
+                hideCursorAfterText={true}
+                typeSpeed={50}
+              />
+            </span>
           </h3>
+          {/* <h3>
+            Freelance <span ref={textRef}></span>
+          </h3> */}
         </div>
         <a href="#portfolio">
           <img src={down} alt="" />
